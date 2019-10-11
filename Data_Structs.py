33c5c7df974b2d22e27974867996bfa9352f7153
Rusty <rww6@sfu.ca>
2019-10-11 10:12:15 -0700

    Completed Queue and Stack Refresh

--- Data_Structs.py
+++ Data_Structs.py
@@ -5,45 +5,52 @@ import util
 
 '''
 Stacks Are LiFo 
-Support: push(), pop()
+push -> append
+pop -> delete back
 '''
 def stackFill(arr):
+    print("Filling Stack...")
     for i in range(0,10):
         arr.append(i)
-
     return arr
 
 def stackCheck(arr):
     print("Init Arr: " + str(arr))
     last = arr[-1]
     arr.pop()
-    print("Removed last item '%s': " % str(last) + str(arr))
-    arr.pop(5)
-    print("Removing key -> 5: " + str(arr))
+    print("Removed item '%s': " % (str(last)) + str(arr))
+    last = arr[-1]
+    arr.pop()
+    print("Removed item '%s': " % (str(last)) + str(arr))
     print("Res Arr: " + str(arr))
-    return arr
+    return(arr)
 
 
 '''
 Queues are FiFo
-Support: put(), get()
+push -> enqueue = add to front
+pop -> dequeue = delete from back
 '''
 def queueFill(arr):
+    print("Filling Queue...")
     for i in range(10):
         arr.push(i)
-        
     return arr
 
 def queueCheck(arr):
     print("Init Arr: " + str(arr))
     arr.pop()
+    print("Dequeue: " + str(arr))
+    arr.pop()
+    print("Dequeue: " + str(arr))
+
     print(arr) 
 
 
 def main():
-    #arr =[]
-    # sFill = stacks(arr)
-    # sChk = stackCheck(sFill)
+    s = util.Stack()
+    sFill = stackFill(s)
+    sChk = stackCheck(sFill)
 
     q = util.Queue()
     qFill = queueFill(q)
